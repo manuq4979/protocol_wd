0dcd993c66fabe7d061d32221b90a30e62002de4
manuq4979 <76844037+manuq4979@users.noreply.github.com>
2023-12-01 02:18:22 +0300

    Build 3
    Исправил логирование.

--- raiting.py
+++ raiting.py
@@ -123,6 +123,10 @@ def read_history_points():
 def add_history_point(text):
 	text2 = None
 	
+	if os.path.exists("history_points.txt"):
+		text2 = read_history_points()
+		text = text2+"_"+text
+	
 	try:
 		with open("history_points.txt", 'w+', encoding='utf-8') as fp:
 			fp.write(text)
@@ -131,9 +135,7 @@ def add_history_point(text):
 		print("\033[31m{}".format("ERROR: ") + "\033[0m{}".format("Увы данные не были записаны! Снова ошибка utf-8! Пробуйте снова!"))
 		return 1
 	
-	if os.path.exists("history_points.txt"):
-		text2 = read_history_points()
-		text = text2+"_"+text
+	
 
 
 def del_history():
